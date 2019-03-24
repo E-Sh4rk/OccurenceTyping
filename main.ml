d3613bbe0b8bb12e5d89ab449a9b841d6e8252a6
E-Sh4rk <esh4rk@gmail.com>
2019-03-24 17:04:40 +0100

    merlin & test

--- main.ml
+++ main.ml
@@ -1,4 +1,17 @@
 
 module CD = Cduce_lib
 
-let _ = Printf.printf "Hello World!"
+let alpha =
+    "a" |> CD.Var.mk |> CD.Types.var |> CD.Types.cons
+
+let alpha_list =
+    let alpha_list = CD.Types.make () in
+
+    let nil_atom = "nil" |> CD.Atoms.V.mk_ascii |> CD.Atoms.atom |> CD.Types.atom in
+    let cons = CD.Types.times alpha alpha_list in
+
+    let descr = CD.Types.cup nil_atom cons in
+    CD.Types.define alpha_list descr ;
+    alpha_list
+
+let _ = CD.Types.Print.printf (CD.Types.descr alpha_list)
