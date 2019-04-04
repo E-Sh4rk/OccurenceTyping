2a292d0bf35a297b3e41e1b62826c3f1c6769a1e
E-Sh4rk <esh4rk@gmail.com>
2019-04-04 14:13:47 +0200

    simlify square operator

--- types_additions.ml
+++ types_additions.ml
@@ -10,11 +10,11 @@ let square f out =
     let res = dnf |> List.map begin
         fun lst ->
             let lst = lst |> List.map begin
-                fun (s,t) ->
-                    if is_empty (cap out t) then (s,false) else (s,true) (* if polymorphism: s should be refined... *)
+                fun (s,t) -> if is_empty (cap out t) then (s,false) else (s,true)
+                (* Does not work with polymorphism (s should be instanciated...) *)
             end in
             let possibles = List.filter (function (_,b) -> b) lst |> List.map fst in
-            let impossibles = List.filter (function (_,b) -> not b) lst |> List.map (fun (t,_) -> neg t) in
-            cap (disj possibles) (conj impossibles)
+            let impossibles = List.filter (function (_,b) -> not b) lst |> List.map fst in
+            diff (disj possibles) (disj impossibles)
     end in
     cap (domain f) (disj res)
