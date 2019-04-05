8e17fb8168cc419f8eb57542d16386ecce97c052
E-Sh4rk <esh4rk@gmail.com>
2019-04-05 15:23:57 +0200

    printer

--- printer.ml
+++ printer.ml
@@ -3,11 +3,30 @@ open Ast
 
 let string_of_varid = string_of_int
 
-let string_of_const t =
-  match t with
+let string_of_const c =
+  match c with
   | Magic -> "magic"
   | Unit  -> "()"
   | Bool b -> string_of_bool b
   | Int i -> string_of_int i
   | Char c -> Printf.sprintf "'%c'" c
   | Atom a -> Cduce.string_of_type a
+
+let rec string_of_expr e =
+  match e with
+  | Const c -> string_of_const c
+  | Var v -> Printf.sprintf "v%s" (string_of_varid v)
+  | Lambda (_,v,e) ->
+    Printf.sprintf "fun %s -> %s" (string_of_varid v) (string_of_expr e)
+  | Ite (e,t,e1,e2) ->
+    Printf.sprintf "if %s is %s then %s else %s" (string_of_expr e) (Cduce.string_of_type t)
+      (string_of_expr e1) (string_of_expr e2)
+  | App (e1, e2) ->
+    Printf.sprintf "(%s) (%s)" (string_of_expr e1) (string_of_expr e2)
+  | Let (v, e1, e2) ->
+    Printf.sprintf "let %s = %s in %s" (string_of_varid v) (string_of_expr e1) (string_of_expr e2)
+  | Pair (e1, e2) ->
+    Printf.sprintf "(%s, %s)" (string_of_expr e1) (string_of_expr e2)
+  | Projection (Fst, e) -> Printf.sprintf "fst (%s)" (string_of_expr e)
+  | Projection (Snd, e) -> Printf.sprintf "snd (%s)" (string_of_expr e)
+  | Debug (str, e) -> Printf.sprintf "debug \"%s\" (%s)" str (string_of_expr e)
--- printer.mli
+++ printer.mli
@@ -1,4 +1,8 @@
 
 open Ast
 
+val string_of_varid : varid -> string
+
 val string_of_const : Cduce.typ const -> string
+
+val string_of_expr : expr -> string
